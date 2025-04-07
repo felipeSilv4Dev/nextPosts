7ba9836e3aec2ce9e1efc1f35135b424560ab7e6
felipeSilv4Dev <felipesilva.developer@gmail.com>
2025-04-07 11:25:32 -0300

    adding cloudnary for images

--- actions/posts.js
+++ actions/posts.js
@@ -1,6 +1,7 @@
 "use server";
 import { redirect } from "next/navigation";
 import { storePost } from "@/lib/posts";
+import { uploadImage } from "@/lib/cloudnary";
 
 export async function createPost(prevState, formData) {
   const title = formData.get("title");
@@ -25,9 +26,17 @@ export async function createPost(prevState, formData) {
       errors,
     };
   }
+  let imageUrl;
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (err) {
+    throw new Error(
+      "Image upload failed, post was not created. Please try again later."
+    );
+  }
 
   await storePost({
-    imageUrl: "",
+    imageUrl,
     title,
     content,
     userId: 1,
