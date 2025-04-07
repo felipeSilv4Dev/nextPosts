92b2c7425a7fac17c0fbabe7cadbd753e3b30a01
felipeSilv4Dev <felipesilva.developer@gmail.com>
2025-04-07 10:04:09 -0300

    first commit

---
+++ components/form-submit.js
@@ -0,0 +1,18 @@
+"use client";
+
+import { useFormState } from "react-dom";
+
+export default function FormSubmit() {
+  const status = useFormState();
+
+  if (status.pending) {
+    return <p>Creating post...</p>;
+  }
+
+  return (
+    <>
+      <button type="reset">Reset</button>
+      <button>Create Post</button>
+    </>
+  );
+}
